10a3dcb2a87498a92ba87494a6b8e591f55789fb
Marios Ioannidis <ioanmar80@gmail.com>
2018-07-10 19:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function ternaryCheckCity(city) {
 
 function switchOnCharmFromTip(tip) {
   if (tip == 'generous') {
-    
+    return 'Thank you so much';
   }
   if (tip == 'not as generous') {
-    
+    return 'Thank you';
   }
 }
